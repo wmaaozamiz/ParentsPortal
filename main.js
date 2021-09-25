fc4b904c4a0bb03a8732cc34a9c0bb872bb41d32
wmaaozamiz <86997985+wmaaozamiz@users.noreply.github.com>
2021-09-25 16:26:04 +0800

    AddFile

---
+++ main.js
@@ -0,0 +1,27 @@
+function myFunction()
+{
+    var un = document.forms["myForm"]["Uname"].value;
+    var pw = document.forms["myForm"]["Pass"].value;
+    if(un=="faculty" && pw=="2021")
+    {
+        window.location.href="dashboard.html";
+    }
+    if(un=="wmaa" && pw=="apare")
+    {
+        window.location.href="student.html";
+    }
+    if(un=="bhem" && pw=="1234")
+    {
+        window.location.href="studentinfo/g9aparebj.html";
+    }
+    
+}
+
+function myFunction2() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
